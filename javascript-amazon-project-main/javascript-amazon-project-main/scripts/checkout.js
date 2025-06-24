1fdefcf8dc058320244b244ea85eed955ead26d8
Isaiah Adebayo <adebayoisaiah256@gmail.com>
2025-06-24 14:33:00 +0100

    Fix price formatting and radio selectors

--- javascript-amazon-project-main/javascript-amazon-project-main/scripts/checkout.js
+++ javascript-amazon-project-main/javascript-amazon-project-main/scripts/checkout.js
@@ -1,5 +1,6 @@
 import {cart} from "../data/cart.js";
 import {products} from '../data/products.js';
+import { formatCurrency } from "./utils/money.js";
 
 let cartSummaryHTML ='';
 cart.forEach((cartItem) => {
@@ -11,8 +12,7 @@ products.forEach((product) => {
   }
 });
 console.log(matchingProduct);
-cartSummaryHTML =
-`<div class="cart-item-container">
+cartSummaryHTML +=`<div class="cart-item-container">
 <div class="delivery-date">
 Delivery date: Tuesday, June 21
 </div>
@@ -26,7 +26,7 @@ Delivery date: Tuesday, June 21
   ${matchingProduct.name}
   </div>
   <div class="product-price">
-    ${matchingProduct.priceCents / 100}
+    ${formatCurrency(matchingProduct.priceCents)}
   </div>
   <div class="product-quantity">
     <span>
@@ -48,7 +48,7 @@ Delivery date: Tuesday, June 21
   <div class="delivery-option">
     <input type="radio" checked
       class="delivery-option-input"
-      name="delivery-option-1">
+      name="delivery-option-${matchingProduct.id}">
     <div>
       <div class="delivery-option-date">
         Tuesday, June 21
@@ -61,7 +61,7 @@ Delivery date: Tuesday, June 21
   <div class="delivery-option">
     <input type="radio"
       class="delivery-option-input"
-      name="delivery-option-1">
+      name="delivery-option-${matchingProduct.id}">
     <div>
       <div class="delivery-option-date">
         Wednesday, June 15
@@ -74,7 +74,7 @@ Delivery date: Tuesday, June 21
   <div class="delivery-option">
     <input type="radio"
       class="delivery-option-input"
-      name="delivery-option-1">
+      name="delivery-option-${matchingProduct.id}">
     <div>
       <div class="delivery-option-date">
         Monday, June 13
