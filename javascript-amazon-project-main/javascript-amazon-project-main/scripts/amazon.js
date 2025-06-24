853f0ebb299cc9feda66c319a467ee1471a7fba5
Isaiah Adebayo <adebayoisaiah256@gmail.com>
2025-06-24 11:00:39 +0100

    Make the cart quantity interactive

--- javascript-amazon-project-main/javascript-amazon-project-main/scripts/amazon.js
+++ javascript-amazon-project-main/javascript-amazon-project-main/scripts/amazon.js
@@ -75,6 +75,13 @@ cart.push({
   productId: productId,
   quantity: 1
 })}
-console.log(cart)
+
+
+let cartQuantity = 0;
+cart.forEach((item) => {
+  cartQuantity += item.quantity;
+});
+
+document.querySelector(".js-cart-quantity").innerHTML = cartQuantity;
 })
 });
