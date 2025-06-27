0da01bade287f369b1a26f598298a9c7597fe9c4
Isaiah Adebayo <adebayoisaiah256@gmail.com>
2025-06-27 16:33:56 +0100

    Save cart in Local storage

--- javascript-amazon-project-main/javascript-amazon-project-main/data/cart.js
+++ javascript-amazon-project-main/javascript-amazon-project-main/data/cart.js
@@ -1,4 +1,7 @@
-export let cart = [
+export let cart = JSON.parse(localStorage.getItem('cart'));
+
+if(!cart){
+    cart =  [
   {
     productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     quantity: 2
@@ -9,6 +12,11 @@ export let cart = [
     },
 
 ];
+}
+
+function saveToStorage(){
+  localStorage.setItem('cart', JSON.stringify(cart));
+}
 export function addToCart(productId){
 let matchingItem;
 cart.forEach((cartItem) => {
@@ -23,6 +31,8 @@ cart.push({
   productId: productId,
   quantity: 1
 })}
+
+saveToStorage();
 };
 
 export function removeFromCart(productId){
@@ -33,4 +43,5 @@ cart.forEach((cartItem) => {
   }
 });
 cart = newCart;
+saveToStorage();
 }
